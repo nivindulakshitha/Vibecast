0246c525cb0e7e5218b66adc31e8984cb6cb5ddf
codingdesk-dev <mail.codingdesk@gmail.com>
2024-04-14 21:21:39 +0530

    Update package.json and functions/meta.js files

--- functions/meta.js
+++ functions/meta.js
@@ -1,17 +1,31 @@
-const Chromium = require("chrome-aws-lambda")
+
 const puppeteer = require("puppeteer-core")
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const chromium = require('chromium');
+//  require('chromedriver');
 
 exports.handler = async function (req, res) {
-    const browser = await puppeteer.launch({
-        args: [
-            '--no-sandbox',
-        ]
-    })
+    let options = new chrome.Options();
+    options.setChromeBinaryPath(chromium.path);
+    options.addArguments('--headless');
+    options.addArguments('--no-sandbox');
+    options.addArguments('--disable-gpu');
+    options.addArguments('--window-size=1280,960');
 
-    const page = await browser.newPage()
-    await page.goto("https://spotifymate.com/")
+    const browser = await new webdriver.Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(options)
+        .build();
 
-    await page.focus("#url")
+    await browser.get("https://spotifymate.com/")
+    await browser.findElement(webdriver.By.id("url")).sendKeys("https://open.spotify.com/track/1w9L6dvttZalWd8XqFYvSa?si=1bb8cc8c4387467a")
+    await browser.findElement(webdriver.By.id("send")).click()
+    await browser.findElement(webdriver.By.xpath("//div[@class='abuttons']/a")).getAttribute("href").then((downloadHref) => {
+        console.log(downloadHref);
+    });
+
+/*     await page.focus("#url")
     await page.keyboard.type("https://open.spotify.com/track/1w9L6dvttZalWd8XqFYvSa?si=1bb8cc8c4387467a")
 
     await page.click("#send")
@@ -29,7 +43,41 @@ exports.handler = async function (req, res) {
         "author": "John Doe",
         "description": "This is a sample API",
         "title": downloadHref
-    }))
+    })) */
 
     browser.close()
 }
+
+/* const fs = require('fs');
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const chromium = require('chromium');
+//require('chromedriver');
+
+async function start() {
+    let options = new chrome.Options();
+    options.setChromeBinaryPath(chromium.path);
+    options.addArguments('--headless');
+    options.addArguments('--disable-gpu');
+    options.addArguments('--window-size=1280,960');
+
+    const driver = await new webdriver.Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(options)
+        .build();
+
+    await driver.get('http://google.com');
+    console.log('Hello Google!');
+    await takeScreenshot(driver, 'google-start-page');
+
+    await driver.quit();
+}
+
+async function takeScreenshot(driver, name) {
+    await driver.takeScreenshot().then((data) => {
+        fs.writeFileSync(name + '.png', data, 'base64');
+        console.log('Screenshot is saved');
+    });
+}
+
+start(); */
