49b361f2a7680485ef928e6728376aaa1aab7a66
codingdesk-dev <mail.codingdesk@gmail.com>
2024-04-15 09:36:47 +0530

    Fix processing logic in scrapeAndUpdateRoomData function

--- scrape.js
+++ scrape.js
@@ -7,7 +7,6 @@ const chromium = require('chromium');
 async function scrapeAndUpdateRoomData(roomData, urlId, spotifyUrl) {
     let browser;
     try {
-        console.log('Scraping Spotify URL:', spotifyUrl);
         browser = await puppeteer.launch({ executablePath: chromium.path });
         const page = await browser.newPage();
 
@@ -20,13 +19,19 @@ async function scrapeAndUpdateRoomData(roomData, urlId, spotifyUrl) {
         const downloadHref = await page.$eval(".abuttons a", element => element.getAttribute("href"));
 
         // Update 'downloadHref' in the JSON content
+        console.log(`Download link for id ${urlId}:`, downloadHref);
         roomData.designed = roomData.designed || {};
+        roomData.designed = roomData.designed[urlId] || "";
         roomData.designed[urlId] = downloadHref;
 
         // Close browser and return downloadHref
         return downloadHref;
     } catch (err) {
         console.error('Error during web scraping:', err);
+        const index = roomData.processing.indexOf(urlId);
+        if (index > -1) {
+            roomData.processing.splice(index, 1);
+        }
         throw err;
     } finally {
         if (browser) {
